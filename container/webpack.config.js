7566fa074cc69bd34e7f6f6c958ae67b8a3d85ca
Sumit Balyan <balyan.sumit9@gmail.com>
2023-05-01 14:14:43 +0530

    initial commit

---
+++ container/webpack.config.js
@@ -0,0 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 1000,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        barchart: "barchart@http://localhost:1001/remoteEntry.js",
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
+};
